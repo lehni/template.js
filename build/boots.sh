83fbbb0b209c45567d9594bfc1130ea2eb42458f
Jürg Lehni <juerg@scratchdisk.com>
2011-03-04 18:01:27 +0000

    Add build scripts for dependent projects, ugly for now as folders are hard-coded.

---
+++ build/boots.sh
@@ -0,0 +1,2 @@
+./build.sh stripped
+cp ../out/Template-helma.js ~/Development/Helma/Boots/base/helma/Global/Template.js
---
+++ build/scriptographer.sh
@@ -0,0 +1,5 @@
+./build.sh stripped
+# cp ../out/Template-rhino.js ~/Development/Scriptographer/scriptographer/src/js/core/lib/Template.js
+
+# doclet
+cp ../out/Template-rhino.js ~/Development/Scriptographer/scriptographer/src/js/doclet/lib/Template.js
